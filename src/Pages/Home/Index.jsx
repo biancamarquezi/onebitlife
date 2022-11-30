b1853671a6dbf09346d70c383fed2de1d2d663ea
Bianca Marquezi <bianca.nml@hotmail.com>
2022-11-30 14:43:25 -0300

    Barra de status e botões de hábitos

--- src/Pages/Home/Index.jsx
+++ src/Pages/Home/Index.jsx
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigation } from "@react-navigation/native";
 import { ScrollView, StyleSheet, View, Text } from "react-native";
 
 import LifeStatus from "../../Components/Common/LifeStatus";
+import StatusBar from "../../Components/Home/StatusBar";
+import CreateHabit from "../../Components/Home/CreateHabit";
 
 export default function Home() {
   const navigation = useNavigation();
+  const [mindHabit, setMindHabit] = useState();
+  const [moneyHabit, setMoneyHabit] = useState();
+  const [bodyHabit, setBodyHabit] = useState();
+  const [funHabit, setFunHabit] = useState();
 
   function handleNavExplanation() {
     navigation.navigate("AppExplanation");
@@ -17,6 +23,11 @@ export default function Home() {
         <View style={{ alignItems: "center" }}>
           <Text style={styles.dailyChecks}>❤️ 20 dias - ✔️ 80 checks</Text>
           <LifeStatus />
+          <StatusBar />
+          <CreateHabit habitArea="Mente" borderColor="#90B7F3" />
+          <CreateHabit habitArea="Financeiro" borderColor="#85BB65" />
+          <CreateHabit habitArea="Corpo" borderColor="#FF0044" />
+          <CreateHabit habitArea="Humor" borderColor="#FE7F23" />
         </View>
         <Text
           style={styles.explanationText}
@@ -24,7 +35,7 @@ export default function Home() {
             handleNavExplanation();
           }}
         >
-          Ver explicação novamente
+          Ver explicações novamente
         </Text>
       </ScrollView>
     </View>
